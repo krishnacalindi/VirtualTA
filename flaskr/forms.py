9d779ff86cf25d193954b4bad133cc4a8dc5655b
Krishna Calindi <kxc190048@utdallas.edu>
2024-03-05 21:13:38 -0600

    removed PyJWT from venv, added strong password val

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -13,6 +13,22 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired(), Length(min=5, max=15, message="Length of password must be 4-15 characters."), EqualTo('confirm', message="Passwords must match.")])
     confirm = PasswordField('Repeat password',  validators=[DataRequired(), Length(min=5, max=15, message="Length of password must be 4-15 characters.")])
     submit = SubmitField('Register')
+    def validate_password(self, password):
+        lowercaseVal = False
+        uppercaseVal = False
+        specialcharVal = False
+        specialChars = '[@_!#$%^&*()<>?/\|}{~:]'
+        for c in password.data:
+            if c.islower():
+                lowercaseVal = True
+            elif c.isupper():
+                uppercaseVal = True
+            else:
+                for specialChar in specialChars:
+                    if specialChar == c:
+                        specialcharVal = True
+        if not (lowercaseVal and uppercaseVal and specialcharVal):
+            raise ValidationError('Not a strong password')
 
 class DFAForm(FlaskForm):
     otp = StringField('Secret code', validators=[DataRequired()])
